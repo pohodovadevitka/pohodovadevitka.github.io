3a7c12f6bd9c5e7e346d21f5560596524e6eddf5
Jaroslav Novotný <jaroslav.novotny.84@gmail.com>
2018-12-18 07:06:30 +0100

    lazy loading of sponsor's images

--- js/main.js
+++ js/main.js
@@ -1,37 +1,11 @@
-requirejs.config({
-  baseUrl: "js",
-  paths: {
-    bootstrap: "https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.bundle.min",
-    jquery: "https://code.jquery.com/jquery-3.3.1"
-  }
-});
+$(document).ready(() => {
+  // load sponsors background images
+  const elements = document.querySelectorAll('#menu-sponsors a[data-background-image]')
 
-require(["bootstrap"], function (bootstrap) {
-  /*
-  $('[data-toggle="popover"]').popover();
-
-  $(".dropdown-menu a.dropdown-toggle").on("click", function(e) {
-    if (
-      !$(this)
-        .next()
-        .hasClass("show")
-    ) {
-      $(this)
-        .parents(".dropdown-menu")
-        .first()
-        .find(".show")
-        .removeClass("show");
+  for (let i = 0; i < elements.length; i++) {
+    const src = elements[i].getAttribute("data-background-image");
+    if (src) {
+      elements[i].style.backgroundImage = "url('../images/sponsors/" + src + "')"
     }
-    const $subMenu = $(this).next(".dropdown-menu");
-    $subMenu.toggleClass("show");
-
-    $(this)
-      .parents("li.nav-item.dropdown.show")
-      .on("hidden.bs.dropdown", function(e) {
-        $(".dropdown-submenu .show").removeClass("show");
-      });
-
-    return false;
-  });
-  */
-});
+  }
+})
