969704c6b7c446dbbd057d7ed0eaa68541b586f2
Jaroslav Novotný <jaroslav.novotny.84@gmail.com>
2019-07-14 08:59:29 +0200

    wrong regular

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ $(document).ready(() => {
     const title = elements[i].getAttribute("title");
     if (src) {
       elements[i].innerHTML = `<picture>
-        <source srcset="../images/sponsors/${src.replace('\.\w$', '.webp')}" type="image/webp">
+        <source srcset="../images/sponsors/${src.replace(/\.\w+$/, '.webp')}" type="image/webp">
         <img src="../images/sponsors/${src}" alt="${title}">
       <picture>`
     }
