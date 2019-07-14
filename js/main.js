1fb1c09649db3e6eafcf2bf5788c3158b17a2241
Jaroslav Novotný <jaroslav.novotny.84@gmail.com>
2019-07-14 08:49:59 +0200

    experiment with sponsors

--- js/main.js
+++ js/main.js
@@ -4,8 +4,12 @@ $(document).ready(() => {
 
   for (let i = 0; i < elements.length; i++) {
     const src = elements[i].getAttribute("data-background-image");
+    const title = elements[i].getAttribute("title");
     if (src) {
-      elements[i].style.backgroundImage = "url('../images/sponsors/" + src + "')"
+      elements[i].innerHTML = `<picture>
+        <source srcset="../images/sponsors/${src.replace('\.\w$', '.webp')}" type="image/webp">
+        <img src="../images/sponsors/${src}" alt="${title}">
+      <picture>`
     }
   }
 })
